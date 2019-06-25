c10e6a090c15281a334a387828eba848902d0e30
tomexar <tommyexar@gmail.com>
2019-06-25 11:03:25 -0600

    made a master login

--- data/seeds/002-users.js
+++ data/seeds/002-users.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {username: 'Tom', password: 'drowssap'},
+        {username: 'Tom', password: '$2a$04$6f4wiI1kqT5k9T5haei7RelGUyuJJHOOWfGHJAAAO9S/HUySyGS96'},
         {username: 'Joe', password: 'Lakers77'},
         {username: 'User', password: 'seed pw3'},
       ]);
